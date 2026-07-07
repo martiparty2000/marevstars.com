638cb649ad5e64180a54aa865bcdc8efbeb2b2c8
Martin Milenov Dimitrov <martin.dimitrov1020@gmail.com>
2026-07-08 00:01:43 +0300

    womp

--- marev_stars_site/urls.py
+++ marev_stars_site/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+# Ако искаш началната страница да води към нещо конкретно:
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', include('team.urls')),  # Connects your 'team' app routing table
+    path('', include('team.urls')), # Това ще зареди URL-ите от приложението ти 'team'
+    path('admin/', admin.site.urls),
 ]
 
