57840ca08b23641a40705ea3529bca6cef413efe
Martin Milenov Dimitrov <martin.dimitrov1020@gmail.com>
2026-07-08 15:10:29 +0300

    Добавяне на импорт за add_user_to_sheet във views.py

--- team/gsheets.py
+++ team/gsheets.py
@@ -1,11 +1,15 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+import os
+
 def get_sheet():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     
-    # Добавяме # type: ignore за да спрем грешките на Pylance
-    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope) # type: ignore
+    # Проверяваме дали сме в Render (/etc/secrets/) или на твоя компютър
+    creds_path = '/etc/secrets/credentials.json' if os.path.exists('/etc/secrets/credentials.json') else 'credentials.json'
+    
+    creds = ServiceAccountCredentials.from_json_keyfile_name(creds_path, scope) # type: ignore
     client = gspread.authorize(creds) # type: ignore
     
     sheet = client.open_by_key('1fhwdx7ug04xDHu2YdZzu0SuRKq2RtbHdEhzLP-eQQ0c').sheet1
