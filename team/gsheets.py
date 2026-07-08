fbb9bfa20d695ded31dad292b452ee9f2dfc092d
Martin Milenov Dimitrov <martin.dimitrov1020@gmail.com>
2026-07-08 14:47:39 +0300

    Update gsheets and requirements

--- team/gsheets.py
+++ team/gsheets.py
@@ -1,15 +1,16 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import os
 
 def get_sheet():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-    client = gspread.authorize(creds)
-    # Замени с ID-то от URL на твоята таблица
-    sheet = client.open_by_key('1fhwdx7ugO4xDHu2YdZzuOSuRKq2RtbHdEhzLP-eQQOc').sheet1
+    
+    # Добавяме # type: ignore за да спрем грешките на Pylance
+    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope) # type: ignore
+    client = gspread.authorize(creds) # type: ignore
+    
+    sheet = client.open_by_key('1fhwdx7ug04xDHu2YdZzu0SuRKq2RtbHdEhzLP-eQQ0c').sheet1
     return sheet
 
 def add_user_to_sheet(child_name, parent_email):
     sheet = get_sheet()
-    sheet.append_row([child_name, parent_email, "0", "Не"])
+    sheet.append_row([child_name, parent_email, "0", "He"])
