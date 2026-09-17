54701536f6d221c9097ee00194b1ed90773079f7
Martin Milenov Dimitrov <martin.dimitrov1020@gmail.com>
2026-09-17 18:56:05 +0300

    clear up some files

--- team/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class TeamConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'team'
-
-    def ready(self):
-        # Тук импортираш сигналите, за да работят
-        import team.signals
