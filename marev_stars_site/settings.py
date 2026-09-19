6040d99d893e0355cc0a8a3543ed4c482df11072
martiparty2000 <martin.dimitrov1020@gmail.com>
2026-09-19 22:21:55 +0300

    Configure Support email notification settings

--- marev_stars_site/settings.py
+++ marev_stars_site/settings.py
@@ -65,3 +65,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'team/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Email notifications for Support tickets that are waiting for a consultant.
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+SUPPORT_NOTIFICATION_EMAIL = os.environ.get('SUPPORT_NOTIFICATION_EMAIL', '')
